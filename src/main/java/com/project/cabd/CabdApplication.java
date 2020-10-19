49903c1a64c4bf4d8e9c9c5a4ce3bdd9fb3160c4
Baraian Iulia Maria <iuliabaraian@yahoo.com>
2020-10-19 12:10:08 +0300

    first commit

---
+++ src/main/java/com/project/cabd/CabdApplication.java
@@ -0,0 +1,13 @@
+package com.project.cabd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabdApplication.class, args);
+	}
+
+}
